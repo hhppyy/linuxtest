8c4dd3e78421d17f51df261bde98cfa3528bbe12
hpy <741841851@qq.com>
2020-06-05 16:07:53 +0800

    allure_test2

--- test_allure.py
+++ test_allure.py
@@ -19,13 +19,13 @@ class TestEditPage():
     """编辑页面"""
 
     @allure.story("这是一个xx的用例")
-    def test_1(self,login):
+    def test_1(self):
         """用例描述：先登录，再去执行xxx"""
         step_1()
         step_2()
         print("xxx")
     @allure.story("这是一个yyy的用例")
-    def test_2(self,login):
+    def test_2(self):
         """用例描述：先登录，在执行yyy"""
         print("yyyy")
 
